414b86293c14f7aa045e60ead9c5ba8cfc5a107c
dnafs8 <dnafs8@gmail.com>
2020-10-20 22:28:40 +0530

    Added shell script case.sh

---
+++ case.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Using case construct
+echo -n "Enter a letter or a digit: "
+read a
+
+case "$a" in
+[[:lower:]] ) echo "$a is a lowercase letter";;
+[[:upper:]] ) echo "$a is an uppercase letter";;
+[0-9] ) echo "$a is a digit";;
+* ) echo "$a is a special character";;
+esac
+
+exit 0
